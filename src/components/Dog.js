af85e0b6ff47671f7b8a8e4360a6236006f82eef
paulonteri <onteripaul@gmail.com>
2020-06-16 12:44:34 +0300

    fix Dog component syntax

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,54 +1,62 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
- function Dog(){
-     return(
-		 <div id="dogsection">
-			 <div classname = "post.featured">
-				 <p>
-					 Dogs are man's Bestfriend and that why we want to  ensure everyone
-					 got a Bestfriend.
-					 <ul>
-					 <li>Heathly with proof.</li>
-					 <li>Heathly with proof.</li>
-					 <li>Heathly with proof.</li>
-					 <li>Heathly with proof.</li>
-					 </ul>
-				 </p>
-			 </div>
-		<div className="mee"> 
-		<div id="doggy">
-			<div className="imgbox">
-			   
-			</div>
-			<div className="boxcontent">
-			   <h2>LILY</h2>
-			   <h3>FEMALE</h3><br></br><br></br>
-			   <button><Link to="">LEARN MORE</Link></button>
-			</div>
-		</div>
-		<div id="webby">
-			<div className="imgbox1">
-			   
-			</div>
-			<div className="boxcontent">
-			<h2>LILY</h2>
-			   <h3>FEMALE</h3><br></br><br></br>
-			   <button><Link to="">LEARN MORE</Link></button>
-			</div>
-		</div>
-		<div id="cybey">
-			<div className="imgbox2">
-			   
-			</div>
-			<div className="boxcontent">
-			<h2>LILY</h2>
-			   <h3>FEMALE</h3><br></br><br></br>
-			   <button><Link to="">LEARN MORE</Link></button>
-			</div>
-		</div>
-		</div>
-		</div>
-     );
- }
+import React from "react";
+import { Link } from "react-router-dom";
+function Dog() {
+  return (
+    <div id="dogsection">
+      <div className="post.featured">
+        <div>
+          <p>
+            Dogs are man's Bestfriend and that why we want to ensure everyone
+            got a Bestfriend.
+          </p>
+          <ul>
+            <li>Heathly with proof.</li>
+            <li>Heathly with proof.</li>
+            <li>Heathly with proof.</li>
+            <li>Heathly with proof.</li>
+          </ul>
+        </div>
+      </div>
+      <div className="mee">
+        <div id="doggy">
+          <div className="imgbox"></div>
+          <div className="boxcontent">
+            <h2>LILY</h2>
+            <h3>FEMALE</h3>
+            <br></br>
+            <br></br>
+            <button>
+              <Link to="">LEARN MORE</Link>
+            </button>
+          </div>
+        </div>
+        <div id="webby">
+          <div className="imgbox1"></div>
+          <div className="boxcontent">
+            <h2>LILY</h2>
+            <h3>FEMALE</h3>
+            <br></br>
+            <br></br>
+            <button>
+              <Link to="">LEARN MORE</Link>
+            </button>
+          </div>
+        </div>
+        <div id="cybey">
+          <div className="imgbox2"></div>
+          <div className="boxcontent">
+            <h2>LILY</h2>
+            <h3>FEMALE</h3>
+            <br></br>
+            <br></br>
+            <button>
+              <Link to="">LEARN MORE</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
- export default Dog;
+export default Dog;
