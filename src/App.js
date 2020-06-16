460769ff48593ed61b51a44e7dc2cfd50bdb22d2
paulonteri <onteripaul@gmail.com>
2020-06-16 12:40:11 +0300

    remove BrowserRouter - not needed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import React  from 'react';
-import  Header  from './components/Header';
-import Footer from './components/Footer';
-import Content from './components/Content';
-import { BrowserRouter }  from "react-router-dom";
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
 
-export default function App  () {
-  
-
-       return(
-   
-        
-        <div className = "is-preload">
-        <div id="wrapper" className="fade-in">
-         <BrowserRouter>
-          <Header />
-          <Content />
-          <Footer />
-          </BrowserRouter> 
-
-          
-        </div>
-     
-   </div>
-  
-       );
- 
-  
+export default function App() {
+  return (
+    <div className="is-preload">
+      <div id="wrapper" className="fade-in">
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </div>
+  );
 }
-
-
-
